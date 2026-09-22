3e1212d1b2f1082baaa96005664431f77d9d0092
elis-ctrl <elis_emb@hotmail.com>
2026-09-22 20:43:14 -0300

    crição componentes

---
+++ src/app/component/produto-lista/produto-lista.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-produto-lista',
+  styleUrl: './produto-lista.css',
+  templateUrl: './produto-lista.html',
+})
+export class ProdutoLista {}
