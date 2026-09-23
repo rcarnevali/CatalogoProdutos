2fadf0256fdce5bdf81fa5651d80f6d7995b0590
elis-ctrl <elis_emb@hotmail.com>
2026-09-22 21:02:15 -0300

    produto-lista

--- src/app/component/produto-lista/produto-lista.ts
+++ src/app/component/produto-lista/produto-lista.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ListaService } from '../../services/lista.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterLink],
   selector: 'app-produto-lista',
   styleUrl: './produto-lista.css',
   templateUrl: './produto-lista.html',
 })
-export class ProdutoLista {}
+export class ProdutoLista implements OnInit {
+  constructor(private listaService: ListaService) { }
+
+  sugestoes: any[] = [];
+
+  formItem = new FormGroup({
+    nome: new FormControl('', Validators.required),
+    quantidade: new FormControl(1, [Validators.required, Validators.min(1)]),
+  });
+
+  ngOnInit(): void {
+    this.listaService.buscarSugestoes().subscribe({
+      next: (data) => this.sugestoes = data,
+      error: (error) => console.error('Deu ruim:', error),
+    });
+  }
+
+  enviar(){
+    if (this.formItem.valid) {
+      this.listaService.adicionarItem(
+        this.formItem.value.nome!,
+        this.formItem.value.quantidade!
+      );
+      this.formItem.reset({ nome: '', quantidade: 1 });
+    }
+
+  }
+}
